f92b226e13b8b770103a9f52fdf92eeb1a08298c
AnkiPatel <ankitaddit@gmail.com>
2024-09-23 09:20:38 +0530

    Adding more understanding

--- src/com/ankita/patel/amdashmethodspassbyvalues/PassByValueLearning.java
+++ src/com/ankita/patel/amdashmethodspassbyvalues/PassByValueLearning.java
@@ -9,26 +9,26 @@ public class PassByValueLearning {
 
 	public static void main(String[] args) {
 		int variableInMain = 10;
-		System.out.println("Value in main function: point 1: " + variableInMain);
+		System.out.println("Value in main function: point 1: " + variableInMain); //10
 		changeIntValue(variableInMain);
 		
-		System.out.println("Value in main function: point 2: " + variableInMain);
+		System.out.println("Value in main function: point 2: " + variableInMain); //10
 		variableInMain = changeInt(variableInMain); // return value again getting catch in same variable
 		//hence even though local values get change in function, changes are seems reflecting
 		
-		System.out.println("Value in main function: point 3: " + variableInMain);
+		System.out.println("Value in main function: point 3: " + variableInMain); // 44
 		
 		int[] iarr = {1,2,3};
-		System.out.println("Array value : point 1: " + Arrays.toString(iarr));
+		System.out.println("Array value : point 1: " + Arrays.toString(iarr)); //[1, 2, 3]
 		changeArraySecondElmValue(iarr); // Here in this method we gonna change the value of 
 		//second element of array. in Java when you change the element value of array in function, it 
 		//considered as pass by reference. hence caller function will also get the impact. 
 		
-		System.out.println("Array value : point 2: " + Arrays.toString(iarr));
+		System.out.println("Array value : point 2: " + Arrays.toString(iarr)); // [1, 1111, 3]
 		
 		clearArray(iarr); // Here iarr pass as value. In this function we are not touching individual
 		//elements
-		System.out.println("Array value : point 3: " + Arrays.toString(iarr));
+		System.out.println("Array value : point 3: " + Arrays.toString(iarr));  // [1, 1111, 3]
 		
 		
 		//For user defined class, it works as pass by reference. 
@@ -36,6 +36,13 @@ public class PassByValueLearning {
 		System.out.println("Before passing to function: " + newObj);
 		workOnObject(newObj);
 		System.out.println("After passing to function: " + newObj);
+
+		//
+		Integer myInt = 89;
+		System.out.println("myInt before passing to funciton: " + myInt);
+		workOnIntObj(myInt);
+		System.out.println("myInt after passing to funciton: " + myInt);
+
 
 	}
 	
@@ -63,6 +70,10 @@ public class PassByValueLearning {
 		pMyClassObj.setName("rhea");
 		pMyClassObj.setCounter(45);
 		System.out.println("Inside function : " + pMyClassObj);
+	}
+
+	private static void workOnIntObj(Integer p) {
+		p = 56;
 	}
 
 }
